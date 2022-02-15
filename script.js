e5c2c1848a5ec66a95f1364ccf64702ffc537891
Pranav Vinayak Jadhav <pj8841466@gmail.com>
2022-02-15 20:35:10 +0530

    Commplete

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function TipPerson(){
         tipPercent = tipPercent/100;
         let tipValue = Totalbill*tipPercent;
         let PersonTipValue = (tipValue/tipPepole).toFixed(2);
-        let TotalPerson = (Totalbill/tipPepole)+(PersonTipValue-'0');
+        let TotalPerson = parseFloat((Totalbill/tipPepole)+(PersonTipValue-'0')).toFixed(2);
         console.log(PersonTipValue);
         console.log(TotalPerson);
         tipAmout.innerHTML = 
